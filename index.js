b158a8e8dc2f8083916fd2a62f425d4e9eff9814
Rafał Wyszomirski <ralf@desmart.com>
2017-01-23 16:01:16 +0100

    Improvement(styles, js): Give more control of how the the hud component looks like to the user

--- index.js
+++ index.js
@@ -17,130 +17,163 @@ import {
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  mainContainer: {flex: 1, justifyContent: "center", alignItems: "center", position:"absolute", top: 0, bottom: 0, left: 0, right: 0},
-  hudContainer: {justifyContent:"center", alignItems: "center", width:80, height:80, borderRadius: 5}
-})
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  hudContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 class HudView extends React.Component {
   constructor(props) {
-     super(props);
-     this.state = {
-       fadeDuration: this._getFadeDuration(),
-       isVisible: false,
-       isRotating: false,
-       icon: null,
-       fadeAnim: new Animated.Value(0),
-       rotationAnim: new Animated.Value(0),
-     };
-   }
+    super(props);
+    this.state = {
+      fadeDuration: this._getFadeDuration(),
+      isVisible: false,
+      isRotating: false,
+      icon: null,
+      fadeAnim: new Animated.Value(0),
+      rotationAnim: new Animated.Value(0),
+    };
+  }
 
-   _hexToRgb(hex){
-       hex = hex.replace('#','');
-       r = parseInt(hex.substring(0,2), 16);
-       g = parseInt(hex.substring(2,4), 16);
-       b = parseInt(hex.substring(4,6), 16);
-       return [r, g, b].join();
-   }
+  _hexToRgb(hex) {
+    hex = hex.replace('#', '');
+    r = parseInt(hex.substring(0, 2), 16);
+    g = parseInt(hex.substring(2, 4), 16);
+    b = parseInt(hex.substring(4, 6), 16);
+    return [r, g, b].join();
+  }
 
-   _fadeIn() {
-     this.setState({isVisible: true})
-     Animated.timing(
-       this.state.fadeAnim,
-       {toValue: 1, duration: this.state.fadeDuration}
-     ).start();
-   }
+  _fadeIn() {
+    this.setState({ isVisible: true })
+    Animated.timing(
+      this.state.fadeAnim,
+      { toValue: 1, duration: this.state.fadeDuration }
+    ).start();
+  }
 
-   _fadeOut() {
-     Animated.timing(
-       this.state.fadeAnim,
-       {toValue: 0, duration: this.state.fadeDuration}
-     ).start(() => {
-       this.setState({isVisible: false})
-     });
-   }
+  _fadeOut() {
+    Animated.timing(
+      this.state.fadeAnim,
+      { toValue: 0, duration: this.state.fadeDuration }
+    ).start(() => {
+      this.setState({ isVisible: false })
+    });
+  }
 
-   _initializeRotationAnimation(isRotating) {
-     this.state.rotationAnim.setValue(0)
-     if (!isRotating && !this.state.isVisible) return;
+  _initializeRotationAnimation(isRotating) {
+    this.state.rotationAnim.setValue(0);
+    if (!isRotating && !this.state.isVisible) {
+      return;
+    }
 
-     Animated.timing(this.state.rotationAnim, {
-            toValue: 1,
-            duration: 800,
-            easing: Easing.linear,
-        }).start(() => {
-          this._initializeRotationAnimation()
-        });
-   }
+    Animated.timing(this.state.rotationAnim, {
+      toValue: 1,
+      duration: 800,
+      easing: Easing.linear,
+    }).start(() => {
+      this._initializeRotationAnimation()
+    });
+  }
 
-   _getHudBackgroundColor() {
-     return this.props.hudBackgroundColor || "#000000";
-   }
+  _getHudBackgroundColor() {
+    return this.props.hudBackgroundColor || "#000000";
+  }
 
-   _getFadeDuration() {
-     return this.props.fadeDuration != null && this.props.fadeDuration >= 0 ? this.props.hudOpacity : 700;
-   }
+  _getFadeDuration() {
+    return this.props.fadeDuration != null && this.props.fadeDuration >= 0 ? this.props.hudOpacity : 700;
+  }
 
-   _getHudOpacity() {
-     return this.props.hudOpacity != null && this.props.hudOpacity >= 0 ? this.props.hudOpacity : 0.8;
-   }
+  _getHudOpacity() {
+    return this.props.hudOpacity != null && this.props.hudOpacity >= 0 ? this.props.hudOpacity : 0.8;
+  }
 
-   _getIconSize() {
-     return this.props.iconSize || 42;
-   }
+  _getIconSize() {
+    return this.props.iconSize || 42;
+  }
 
-   _getIconColor() {
-     return this.props.iconColor || "#FFFFFF";
-   }
+  _getWidthSize() {
+    return this.props.width || 80;
+  }
 
-   _getHudRgbaColor() {
-     const opacity = this._getHudOpacity();
-     const color = this._getHudBackgroundColor();
-     const rgbColor = this._hexToRgb(color);
-     return `rgba(${rgbColor},${opacity})`
-   }
+  _getHeightSize() {
+    return this.props.height || 80;
+  }
 
-   _getSetName() {
-     return this.props.setName ? this.props.setName.toLowerCase() : 'fontawesome';
-   }
+  _getBorderRadiusSize() {
+    return this.props.iconSize || 5;
+  }
+
+  _getIconColor() {
+    return this.props.iconColor || "#FFFFFF";
+  }
+
+  _getHudRgbaColor() {
+    const opacity = this._getHudOpacity();
+    const color = this._getHudBackgroundColor();
+    const rgbColor = this._hexToRgb(color);
+    return `rgba(${rgbColor},${opacity})`
+  }
+
+  _getSetName() {
+    return this.props.setName ? this.props.setName.toLowerCase() : 'fontawesome';
+  }
 
   _getIconComponent(setName) {
     switch (setName) {
       case 'foundation':
-        return Foundation
+        return Foundation;
       case 'zocial':
-        return Zocial
+        return Zocial;
       case 'octicons':
-        return Octicons
+        return Octicons;
       case 'materialicons':
-        return MaterialIcons
+        return MaterialIcons;
       case 'ionicons':
-        return Ionicons
+        return Ionicons;
       case 'evilicons':
-        return EvilIcons
+        return EvilIcons;
       case 'entypo':
-        return Entypo
+        return Entypo;
       default:
         return FontAwesome
     }
   }
 
-   _getInterpolatedRotateAnimation() {
-     return this.state.rotationAnim.interpolate({
-           inputRange: [0, 1],
-         outputRange: ['0deg', '360deg']
-       });
-   }
+  _getInterpolatedRotateAnimation() {
+    return this.state.rotationAnim.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
+  }
 
-   _getHudContainerStyles() {
-     return [styles.hudContainer, {opacity: this.state.fadeAnim}, {backgroundColor: this._getHudRgbaColor()}]
-   }
+  _getHudContainerStyles() {
+    return [
+      styles.hudContainer,
+      { opacity: this.state.fadeAnim },
+      { width: this._getWidthSize() },
+      { height: his._getHeightSize() },
+      { borderRadius: his._getBorderRadiusSize() },
+      { backgroundColor: this._getHudRgbaColor() },
+    ]
+  }
 
   _getContainerStyles() {
-    return [this.props.style, {flex: 1}];
+    return [this.props.style, { flex: 1 }];
   }
 
   _getIconWrapperStyles() {
-    var styles = this.state.isRotating ? {transform: [{rotate: this._getInterpolatedRotateAnimation()}]} : {};
+    var styles = this.state.isRotating ? { transform: [{ rotate: this._getInterpolatedRotateAnimation() }] } : {};
     return styles;
   }
 
@@ -151,7 +184,8 @@ class HudView extends React.Component {
   }
 
   _renderDefaultSpinnerComponent() {
-    return <FontAwesome name="circle-o-notch" size={this._getIconSize()} color={this._getIconColor()}/>
+    return <FontAwesome name="circle-o-notch" size={this._getIconSize()}
+                        color={this._getIconColor()}/>
   }
 
   _renderDefaultSuccessComponent() {
@@ -159,11 +193,12 @@ class HudView extends React.Component {
   }
 
   _renderDefaultErrorComponent() {
-    return <FontAwesome name="exclamation-triangle" size={this._getIconSize()} color={this._getIconColor()}/>
+    return <FontAwesome name="exclamation-triangle" size={this._getIconSize()}
+                        color={this._getIconColor()}/>
   }
 
   _showHud(icon, rotate, hideOnCompletion) {
-    this.setState({isVisible: false, icon: icon, isRotating: rotate})
+    this.setState({ isVisible: false, icon: icon, isRotating: rotate })
     this._initializeRotationAnimation(rotate);
     this._fadeIn();
 
@@ -173,19 +208,21 @@ class HudView extends React.Component {
           this.hide();
           setTimeout(() => {
             resolve();
+          }, this.state.fadeDuration)
         }, this.state.fadeDuration)
-      }, this.state.fadeDuration)
       }
     });
   }
 
   _renderHud() {
-    if (!this.state.isVisible) return;
+    if (!this.state.isVisible) {
+      return;
+    }
 
     return <View style={styles.mainContainer}>
-     <Animated.View style={this._getHudContainerStyles()}>
+      <Animated.View style={this._getHudContainerStyles()}>
         {this._renderIcon()}
-     </Animated.View>
+      </Animated.View>
     </View>
   }
 
@@ -210,7 +247,8 @@ class HudView extends React.Component {
 
   showCustomIcon(setName, iconName, rotate, hideOnCompletion) {
     const _component = this._getIconComponent(setName);
-    const icon = <_component name={iconName} size={this._getIconSize()} color={this._getIconColor()}/>
+    const icon = <_component name={iconName} size={this._getIconSize()}
+                             color={this._getIconColor()}/>;
     return this._showHud(icon, rotate, hideOnCompletion);
   }
 
@@ -235,6 +273,6 @@ HudView.propTypes = {
   successComponent: React.PropTypes.object,
   errorComponent: React.PropTypes.object,
   spinnerComponent: React.PropTypes.object,
-}
+};
 
 module.exports = HudView;
